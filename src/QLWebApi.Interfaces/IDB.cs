b9988fb61ff96fce8f69f8c53b68171fda9a6879
Victor Sinaniev <victor.sinaniev@quality-line.org>
2016-11-03 22:16:39 +0200

    Project created:  - DB class added

---
+++ src/QLWebApi.Interfaces/IDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLWebApi.Interfaces
+{
+    public interface IDB
+    {
+        string GetQLGDBVer();
+    }
+}
---
+++ src/QLWebApi/Controllers/DBController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QLWebApi.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QLWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DBController : Controller
+    {
+        public IDB Db { get; set; }
+
+        public DBController(IDB db)
+        {
+            Db = db;
+        }
+
+        // GET: api/qlgdbVer
+        [Route("qlgdbVer")]
+        public IActionResult GetQLGDBVer()
+        {
+            var ver = Db.GetQLGDBVer();
+
+            return new ObjectResult(ver);
+        }
+    }
+}
